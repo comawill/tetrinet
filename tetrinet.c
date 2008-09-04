15815bc9a0e17f6288aeb6929bc59107ebc18842
Gerfried Fuchs <rhonda@deb.at>
2008-09-04 09:59:57 +0200

    don't write past the end of the array

--- tetrinet.c
+++ tetrinet.c
@@ -141,7 +141,6 @@ void parse(char *buf)
 		winlist[i].team = 0;
 	    s++;
 	    strncpy(winlist[i].name, s, sizeof(winlist[i].name)-1);
-	    winlist[i].name[sizeof(winlist[i].name)] = 0;
 	    winlist[i].points = atoi(t);
 	    if ((t = strchr(t, ';')) != NULL)
 		winlist[i].games = atoi(t+1);
