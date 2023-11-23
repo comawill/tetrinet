f2972d2d73a78164da6296f62b5fb0de347c85b5
Sebastian Willenborg <comawill@wlnbrg.de>
2023-11-23 12:55:44 +0100

    fix duplicated definition of piecedata

--- tetris.h
+++ tetris.h
@@ -50,7 +50,7 @@ typedef struct {
     char shape[4][4];	/* Shape data for the piece */
 } PieceData;
 
-PieceData piecedata[7][4];
+extern PieceData piecedata[7][4];
 
 extern int current_piece, current_rotation;
 
